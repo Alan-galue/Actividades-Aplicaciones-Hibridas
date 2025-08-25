86d170709d38f7769d5d8cf975253a9a81e0e8af
Alan <alan.galue@davinci.edu.ar>
2025-08-25 14:44:23 -0300

    Agregamos las funciones eliminar producto y actualizar producto, hay algunas cosas que son de la parte de npm, pero la actividad 3 esta lista.

--- Actividades/Actividad3/Product.js
+++ Actividades/Actividad3/Product.js
@@ -1,51 +1,77 @@
 const fs = require('fs/promises');
 const path = './data/products.json';
 
-class Product{
+class Product {
     products = [];
-    constructor(products=[]){
-        this.products = products
+    
+    constructor(products = []) {
+        this.products = products;
     }
-    async saveJSON(){
+    
+    async saveJSON() {
         try {
-            const data = JSON.stringify( this.products, null, 2);
-            await  fs.writeFile(path, data);
+            const data = JSON.stringify(this.products, null, 2);
+            await fs.writeFile(path, data);
         } catch (error) {
-            console.log('No se pudo guardar los datos')
+            console.log('No se pudo guardar los datos');
         }
     }
-    async readJSON(){
+    
+    async readJSON() {
         try {
             const data = await fs.readFile(path);
             const products = JSON.parse(data);
-            return products
+            return products;
         } catch (error) {
             console.error('No se pudo leer los datos');
-            return []
+            return [];
         }
     }
-    addProduct(product){
-        // Validar datos!
+    
+    addProduct(product) {
         const id = crypto.randomUUID();
         product.id = id;
         this.products.push(product);
         this.saveJSON();
     }
-    async getProducts(){
+    
+    async getProducts() {
         this.products = await this.readJSON();
-        return this.products
+        return this.products;
     }
-    async getProductById(id){
+    
+    async getProductById(id) {
         this.products = await this.readJSON();
-        const product = this.products.find( item => item.id == id );
+        const product = this.products.find(item => item.id == id);
         return product ? product : {};
     }
-    deleteProductById(id){
-
-    }
-    updateProductById(id, product){
+    
+    deleteProductById(id) {
+        const index = this.products.findIndex(item => item.id === id);
         
+        if (index !== -1) {
+            this.products.splice(index, 1);
+            this.saveJSON();
+            return true;
+        } else {
+            console.log("Producto no encontrado, intente nuevamente");
+            return false;
+        }
+    }
+    
+    updateProductById(id, product) {
+        const index = this.products.findIndex(item => item.id === id);
+        
+        if (index !== -1) {
+            product.id = id;
+            this.products[index] = product;
+            this.saveJSON();
+            return true;
+        } else {
+            console.log("El producto no pudo ser actualizado.");
+            return false;
+        }
     }
 }
 
-module.exports = Product;
+module.exports = Product; 
