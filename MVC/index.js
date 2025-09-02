f3792aebd5efbd407836d6dae90314936c31c4f7
Alan <alan.galue@davinci.edu.ar>
2025-09-02 10:46:05 -0300

    agregamos el .env y otras cosas mas en el index en el router

--- MVC/index.js
+++ MVC/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const dotenv= require('dotenv');
 const app = express();
-const PORT = 1000
-
+const PORT = 1000;
+dotenv.config();
+const urldb= process.env.URI_DB;
+mongoose.connect(urldb)
+const db= mongoose.connection;
+db.on('error', ()=>{
+    console.error('ocurrio un error')
+})
+db.once('open', ()=>{
+    console.log('conexion correcta')
+})
 app.use('/', (request, response) =>  {
 response.send('<h1>API REST</h1>')
 })
 
 app.listen(PORT, () => {
-    console.log('API REST EN EL PUERTO ${PORT}')
+    console.log("API EN EL PUERTO " + PORT )
 })
